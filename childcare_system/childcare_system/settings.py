2741a253252932b84b21d98c6b42d87327d5f7b1
Zambagarrah <132018635+Zambagarrah@users.noreply.github.com>
2026-03-04 08:14:19 +0300

    chore: configure project to use custom User model

--- childcare_system/childcare_system/settings.py
+++ childcare_system/childcare_system/settings.py
@@ -116,3 +116,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Use custom User model
+AUTH_USER_MODEL = 'core.User'
+
