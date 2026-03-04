00c7a0d842e087c1ae1dd0f8c80b7249173ef46c
Zambagarrah <132018635+Zambagarrah@users.noreply.github.com>
2026-03-04 08:23:25 +0300

    feat: implement role-based dashboards for Admin, Caregiver, and Parent

--- childcare_system/core/views.py
+++ childcare_system/core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
+
 
 # Registration view
 def register(request):
@@ -13,3 +15,13 @@ def register(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'register.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    if request.user.role == 'ADMIN':
+        return render(request, 'dashboard_admin.html')
+    elif request.user.role == 'CAREGIVER':
+        return render(request, 'dashboard_caregiver.html')
+    else:
+        return render(request, 'dashboard_parent.html')
+
